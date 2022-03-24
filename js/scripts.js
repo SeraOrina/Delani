87883d8df6da42ae879fabef07a956313f7216b6
Sera <seramokeira@gmail.com>
2022-03-24 11:58:51 +0300

    ft:add button for submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-    $(".img").click(function(){
-        $(".card-1").toggle()
-        $(".card-1").toggle()
+$(document).ready(function () {
+    $("#btn").click(function () {
+        alert("We have received your message!Thank you for your response");
     });
+
+    $("#clickme").click(function(){
+        $("#card-1").toggle()
+    });
+    $()
 });
+
+
+
+
